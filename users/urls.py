ac34afd50be836a890d2f1863877d2bc6ff12136
Aman Goyal <amangoyal8110@gmail.com>
2019-12-30 02:47:11 +0530

    user profile implementation

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register/', users_views.register, name="users-register"),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='users-login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='users-logout')
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='users-logout'),
+    path('profile/', users_views.profile, name='users-profile')
 ]
