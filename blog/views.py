3c6c71b5cf51e64c10e0484319cdc443725b27d3
Aman Goyal <amangoyal8110@gmail.com>
2019-12-31 16:59:22 +0530

    implemented ListView, DetailView

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 def home(request):
@@ -8,6 +9,17 @@ def home(request):
         'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+#home
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+#post detailed 
+class PostDetailView(DetailView):
+    model = Post #<app>/<model_name>_<view type>
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'about'})
